2329434491148e7a088a38e1549660205fd31ae1
memecito <memecito@gmail.com>
2024-08-20 20:37:23 +0200

    primer commit

---
+++ src/app/components/user-generics/user-generics.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-generics',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-generics.component.html',
+  styleUrl: './user-generics.component.css'
+})
+export class UserGenericsComponent {
+
+}
