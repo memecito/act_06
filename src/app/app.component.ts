a8289b6b7f65383f4f73d16205c5dbd4d8443f9a
Antonio <memecito@gmail.com>
2024-08-21 19:28:04 +0200

    trabajo realizado en la biblioteca
    creacion de los datos del interfaz,
    creacion del menu de navegacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuComponent } from './components/menu/menu.component';
+import { NavegacionComponent } from './components/navegacion/navegacion.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MenuComponent, NavegacionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { FormComponent } from '../form/form.component';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [FormComponent],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
