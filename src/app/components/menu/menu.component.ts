6e494546d06e2d8382c253593c1c06e87519bc50
Antonio <memecito@gmail.com>
2024-08-26 19:23:21 +0200

    Rutas
    Generación de rutas para el menú

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormComponent } from '../form/form.component';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [FormComponent],
+  imports: [FormComponent, RouterLink, RouterLinkActive],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.css'
 })
