798b96a897cc4663eccb2910c6f082e2fe05caf1
memecito <memecito@gmail.com>
2024-09-02 01:29:23 +0200

    añadido bootstrap
    Añadido menu de bootstrap,
    Un paginador tambien
    y creacion de paginas en vez de componnets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,9 @@
 import { Routes } from '@angular/router';
-import { UserGenericsComponent } from './components/user-generics/user-generics.component';
 import { FormComponent } from './components/form/form.component';
+import { UsuariosComponent } from './pages/usuarios/usuarios.component';
 
 export const routes: Routes = [
     {path: '', redirectTo:'/inicio',pathMatch:'full'},
-    {path: 'inicio', component: UserGenericsComponent},
+    {path: 'inicio', component:UsuariosComponent},
     {path: 'nuevo', component: FormComponent}
 ];
---
+++ src/app/pages/usuarios/usuarios.component.ts
@@ -0,0 +1,60 @@
+import { Component, inject } from '@angular/core';
+import { User } from '../../interfaces/user.type=interface';
+import { Respuestahttp } from '../../interfaces/responsehttp.type=interface';
+import { UsersService } from '../../services/users.service';
+
+@Component({
+  selector: 'app-usuarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuarios.component.html',
+  styleUrl: './usuarios.component.css'
+})
+export class UsuariosComponent {
+arrUser: User[]=[];
+respuesta: Respuestahttp={page:-1,per_page:-1,total:-1,total_pages:-1,results: this.arrUser};
+UService= inject(UsersService)
+async ngOnInit(){
+  try{
+   this.respuesta= await this.UService.getObjet();
+    console.log(this.respuesta.results)
+    
+    
+  }catch(error){
+    console.log(error)
+  }
+}
+async paginasiguiente(){
+  if(this.respuesta.page<this.respuesta.total_pages){
+    try{
+      console.log(this.respuesta.page+1)
+      this.respuesta= await this.UService.getObjetPage(this.respuesta.page+1);
+       console.log(this.respuesta.results)
+       
+       
+     }catch(error){
+       console.log(error)
+     }
+  }
+  
+ 
+}
+async paginaanterior(){
+  if(this.respuesta.page>1){
+    try{
+      console.log(this.respuesta.page+1)
+      this.respuesta= await this.UService.getObjetPage(this.respuesta.page-1);
+       console.log(this.respuesta.results)
+       
+       
+     }catch(error){
+       console.log(error)
+     }
+  }
+  
+ 
+}
+async editar(id:number){
+  
+}
+}
